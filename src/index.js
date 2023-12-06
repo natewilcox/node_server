1776467ed7d9dd6d65f73b4c90d3d677928758c7
natewilcox@gmail.com <natewilcox@gmail.com>
2023-12-05 20:18:03 -0500

    adding version to print

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const os = require('os');
+const packageJson = require('../package.json');
+
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log("Hello world received a request.");
+  console.log(process.env)
+  const label = process.env.label;
+
+  res.send(`${label} from ${os.hostname()} running version ${packageJson.version}`);
 });
 
 app.listen(port, () => {
